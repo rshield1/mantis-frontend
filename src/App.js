86ef806c4de1d7bb736813fc27f17527cc68be09
Robert Shields <robert.shieldsiv@gmail.com>
2020-09-12 17:23:40 -0400

    added tachyons and particle.js dependency.  Also added name of app to title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+
+
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#76EE00",
+        blur: 5
+      }
+    },
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+    
+  }
+}
+
+
 
 class App extends React.Component {
   render() {
   return (
-    <div className="App">App</div>
+    <div className="App">App
+
+<Particles className='particles'
+      params={particlesOptions} />
+    
+    </div>
   );
 
   }
