555e5f8c6232b3d0a76fef3547e54cb89b1d3ab8
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-10-03 03:48:25 -0400

    added json photo example

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+//bring in useState hook
 
 const Context = React.createContext()
 
 
 const ContextProvider = (props) => {
+    const [allPhotos, setAllPhotos] = useState([])
+    const url ="https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        //get data from api and save to state
+        fetch(url)
+            .then(res => res.json())
+            //save data 
+            .then(data => setAllPhotos(data))
+    }, [])
+        console.log(allPhotos)
+
     return(
-        <Context.Provider value="">
+        <Context.Provider value={{allPhotos: allPhotos}}>
             {props.children}
         </Context.Provider>
     )
